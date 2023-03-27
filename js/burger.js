9c428c95c833ef94e16aa1321be5f97c67850d8c
Krylova Nataliya <natalinebo2014@yandex.ru>
2023-03-27 22:26:49 +0200

    refactor: adjust header and footer styles

--- js/burger.js
+++ js/burger.js
@@ -2,15 +2,18 @@ const BODY = document.body;
 const burgerBtn = document.getElementById('burger-button');
 const headerNav = document.getElementById('header-nav');
 const headerLinks = document.querySelectorAll('.header__link');
+const headerLink = document.querySelectorAll("header__link");
 
 burgerBtn.addEventListener('click', function() {
     headerNav.classList.toggle('header__nav_open');
     burgerBtn.classList.toggle('burger__button_open');
+    BODY.classList.add('block');
 });
 
-if (headerNav.classList.contains('header__nav_open')) {
-    headerNav.addEventListener('click', function(event) {
+headerNav.addEventListener('click', function(event) {
+    if (event.target.classList.contains('header__link')) {
         headerNav.classList.remove("header__nav_open");
         burgerBtn.classList.remove("burger__button_open");
-    });
-}
+        BODY.classList.remove('block');
+    }
+});
